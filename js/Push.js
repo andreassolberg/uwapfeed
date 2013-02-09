e2cee611d2fa9b8cfaadd0056c201a2fcb054267
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2013-02-09 11:39:04 +0100

    minor update

--- js/Push.js
+++ js/Push.js
@@ -6,7 +6,7 @@ define(function(require, exports, module) {
 
 	var Push = function() {
 
-
+		this.register();
 
 	} 
 
@@ -30,7 +30,7 @@ define(function(require, exports, module) {
 	Push.prototype.tokenHandler = function(result) {
 		// Your iOS push server needs to know the token before it can push to this device
 		// here is where you might want to send it the token for later use.
-		alert('device token = '+result)
+		alert('device token = ' + result)
 	}
 
 	return Push;
